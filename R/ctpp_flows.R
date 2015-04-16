47c5f81b01b90e5266d6b8b681ebe8c069a5a66c
Greg Macfarlane <macfarlaneg@pbworld.com>
2015-04-16 14:17:53 -0400

    Made dataset more manageable by concatenating fips codes. Will add helper functions to extract other levels.

--- R/ctpp_flows.R
+++ R/ctpp_flows.R
@@ -23,5 +23,24 @@
 #> df <- RODBC::sqlFetch(channel, "Tract-flows")
 #> 
 #> ctpp_flows <- dplyr::tbl_df(df)
+#>
+#>ctpp_flows <- ctpp_flows %>%
+#>  mutate(
+#>    # fix FIPS code width
+#>    rstate = sprintf("%02d", Residence_State_FIPS_Code),
+#>    wstate = sprintf("%02d", Workplace_State_FIPS_Code),
+#>    rcnty = sprintf("%03d", Residence_County_FIPS_Code),
+#>    wcnty = sprintf("%03d", Workplace_County_FIPS_Code),
+#>    rtract = sprintf("%06d", Residence_Tract_FIPS_Code),
+#>    wtract = sprintf("%06d", Workplace_Tract_FIPS_Code)
+#>  ) %>%
+#>  transmute(
+#>    # concatenate FIPS code to GEOID format
+#>    residence = paste(rstate, rcnty, rtract, sep = ""),
+#>    workplace = paste(wstate, wcnty, wtract, sep = ""),
+#>    flow = EST,
+#>    flow_moe = MOE
+#>  )
+#>
 #> 
 #> save(ctpp_flows, file = "data/ctpp_flows.Rdata")
---
+++ R/ctpp_flows_functions.R
@@ -0,0 +1,17 @@
+#' CTPP tract-to-tract work flows.
+#' 
+#' A dataset containing journey-to-work flow volumes between all census tracts
+#' in the United States collected through the American Community Survey and 
+#' distributed through the Census Transportation Planning Package.
+#' 
+#' @format A data frame with 4,156,426 rows and 4 variables:
+#' \describe{
+#'   \item{residence}{FIPS code of residence tract.}
+#'   \item{workplace}{FIPS code of workplace tract.}
+#'   \item{flow}{Estimate of flow.}
+#'   \item{flow_moe}{Margin of error on flow estimate.}
+#' }
+#'  
+#' @source \url{http://www.fhwa.dot.gov/planning/census_issues/ctpp/data_products/2006-2010_tract_flows/index.cfm}
+#'   
+"ctpp_flows"   
