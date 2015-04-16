a95746991e926d08e21f66df10a3b12f8404c820
Greg Macfarlane <macfarlaneg@pbworld.com>
2015-04-16 13:27:50 -0400

    Wrote script to save ctpp files as R object.

---
+++ R/ctpp_flows.R
@@ -0,0 +1,24 @@
+# Read the CTPP tract flow data into R.
+
+# The RODBC library relies on a usable Access database driver, installed 
+# separately If the script doesn't try downloading the 64-bit driver from
+#
+#     http://www.microsoft.com/en-us/download/details.aspx?id=13255
+# 
+# Installing this through Windows directly may cause conflicts with the existing
+# Office installation. Instead use the command prompt at the download point
+#
+#     AccessDatabaseEngine_x64.exe /passive
+#
+# Then go to regedit  HKEY_LOCAL_MACHINE > SOFTWARE > Microsoft > Office > 
+# 14.0 > Common > FilesPaths >  and delete `mso.dll`.
+
+# point this to the CTPP flows file downloaded
+acc_file <- "L:/CTPP tracts/tract-flows.accdb"
+channel <-  RODBC::odbcConnectAccess2007(acc_file)
+
+df <- RODBC::sqlFetch(channel, "Tract-flows")
+
+ctpp_flows <- dplyr::tbl_df(df)
+
+save(ctpp_flows, file = "data/ctpp_flows.Rdata")
