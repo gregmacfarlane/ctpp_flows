2b67d7f0e2d7c9e312a0f7c3cc4964e6df569f72
Greg Macfarlane <gregmacfarlane@gmail.com>
2015-04-16 20:25:41 -0400

    Added `get_county` helper function.

--- R/fips_functions.R
+++ R/fips_functions.R
@@ -4,17 +4,42 @@
 #' string.
 #' 
 #' @param fips A character vector of complete FIPS strings.
-#' 
 #' @return a character vector with the 2-digit state code.
-#' 
 #' @examples 
-#' get_state(ctpp_flows$residence)
+#' get_state(ctpp_flows$residence[1:10])
 #' 
 #' @export
-#'
 #' @seealso \code{\link{get_county}}, \code{\link{get_tract}} 
 #' 
 get_state <- function(fips){
   # digits 1 and 2 have state FIPS code
-  substr(fips, 1,2)
+  substr(fips, 1, 2)
+}
+
+#' Get the county-level FIPS code
+#'
+#' \code{get_county} returns the 3-digit county FIPS code from a complete FIPS
+#' string.
+#' 
+#' @param fips A character vector of complete FIPS strings.
+#' @param with_state Include the 2-digit state code? Default 
+#'   \code{with_state = FALSE}
+#' @return a character vector with the 3-digit county code, unless 
+#'   \code{with_state = TRUE} in which case a 5-digit state and county code 
+#'   concatenation.
+#' @examples 
+#' get_county(ctpp_flows$residence[1:10])
+#' get_county(ctpp_flows$residence[1:10], with_state = TRUE)
+#' 
+#' @export
+#' @seealso \code{\link{get_county}}, \code{\link{get_tract}} 
+#' 
+get_county <- function(fips, with_state = FALSE){
+  if(with_state){
+    # digits 1 and 2 have state FIPS code
+    substr(fips, 1, 5)
+  } else {
+    # digits 3 through 5 have county FIPS code
+    substr(fips, 3, 5)
+  }
 }
